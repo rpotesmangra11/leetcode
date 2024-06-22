6d2caa253e6619feebaa1c5631cf29119a6c228f
Ravi Potes Mangra <potesmangrar11@gmail.com>
2024-06-21 23:39:41 -0500

    findDigits.js practice problem added

---
+++ findDigits.js
@@ -0,0 +1,20 @@
+// Find Number of Digits in Number
+// Create a function that will return an integer number corresponding to the amount of digits in the given integer num.
+
+// Examples
+// num_of_digits(1000) ➞ 4
+
+// num_of_digits(12) ➞ 2
+
+// num_of_digits(1305981031) ➞ 10
+
+// num_of_digits(0) ➞ 1
+// Notes
+// Try to solve this challenge without using strings!
+
+let num_of_digits = (x) => {
+    let count = 0
+  for (let i = 0; x > 0; i++) {
+    
+  }
+};
